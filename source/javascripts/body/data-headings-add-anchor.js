c3bf5d1ffa9daa51292b10fcfc2b0e578d9594ea
Javier Artero <javier.artero@gmail.com>
2018-02-25 13:11:02 +0100

    Component badge (#52)
    * fix file name
    
    * bug h inside examples
    
    * alerts title
    
    * fix heading styles
    
    * buttons title
    
    * add badge
    
    * fix name

--- source/javascripts/body/data-headings-add-anchor.js
+++ source/javascripts/body/data-headings-add-anchor.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  var $headings = $('*[data-headings-add-anchor] :header').not('h4, h5, h6');
+  var $headings = $('*[data-headings-add-anchor] >  :header').not('h4, h5, h6');
 
   $headings.each(function() {
     var link = '#' + $(this).attr('id');
