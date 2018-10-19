70570d11eb8015f021e59f003bfa40af1ebf845f
Javier Artero <javier.artero@gmail.com>
2018-10-19 15:35:27 +0200

    rename extend -> placeholder and grid -> bootstrap-grids (#121)
    * fix links menus
    
    * fix tests
    
    * rebuild
    
    * fix bugs with active page
    
    * rename reset -> resets and extends() -> placeholders()
    
    * change in documentation and rename grid to bootstrap-grids
    
    * change version

--- scripts/tests-type-of.rb
+++ scripts/tests-type-of.rb
@@ -5,15 +5,14 @@ apiData = []
 apiData += YAML::load(File.open('data/api/api_elements.yml')).each {|e| e['file'] ||= '_elements.scss'}
 apiData += YAML::load(File.open('data/api/api_forms.yml')).each {|e| e['file'] ||= '_forms.scss'}
 apiData += YAML::load(File.open('data/api/api_helpers.yml')).each {|e| e['file'] ||= '_helpers.scss'}
-apiData += YAML::load(File.open('data/api/api_icons.yml')).each {|e| e['file'] ||= '_icons.scss'}
-apiData += YAML::load(File.open('data/api/api_reset.yml')).each {|e| e['file'] = '_reset.scss'}
+apiData += YAML::load(File.open('data/api/api_reset.yml')).each {|e| e['file'] = '_resets.scss'}
 apiData += YAML::load(File.open('data/api/api_utilities.yml')).each {|e| e['file'] = '_utilities.scss'}
 apiData += YAML::load(File.open('data/api/api_values.yml')).each {|e| [e['file'] = '_values.scss', e['args'] ||= 0]}
 apiData += YAML::load(File.open('data/api/api_properties.yml')).each {|e| [e['file'] = 'core/_properties.scss', e['args'] ||= 0]}
 
 apiData.each {|e| e['type'] ||= 'block' }
 
-fo = File.open('marscss/scss/tests/type-of.scss', 'w')
+fo = File.open('scss/tests/type-of.scss', 'w')
 
 # main %emoji
 
